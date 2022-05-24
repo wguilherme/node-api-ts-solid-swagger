9088654214978d0c5815f5ae8fe239924c3cdf77
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-05-24 10:47:45 -0300

    chore: setup vscode debug with inspect

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res)=>res.json('works'))
+
+app.post('/courses', (request, response)=>{
+  const {name} = request.body
+  response.json({name})
+})
 
 app.listen(3333, ()=>console.log('Server is runnning at port 3333'))
