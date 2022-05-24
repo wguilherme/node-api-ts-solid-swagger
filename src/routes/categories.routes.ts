6771fc06dc675af4a30b97903e621717624581f5
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-05-24 19:08:53 -0300

    chore: add and config multer upload

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,23 @@
 import { Router } from "express";
+import multer from "multer";
 
-import { CategoriesRespository } from "../modules/cars/repositories/implementations/CategoriesRepository";
 import { createCategoryController } from "../modules/cars/usecases/createCategory";
 import { listCategoryController } from "../modules/cars/usecases/listCategory";
+
+const upload = multer({
+  dest: "./tmp",
+});
 
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
+});
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+  console.log("file", file);
+  return response.send();
 });
 
 categoriesRoutes.get("/", (request, response) => {
