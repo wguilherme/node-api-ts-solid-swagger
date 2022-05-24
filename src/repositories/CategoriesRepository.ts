c12e7c8f181d292aaff520be76868073fd50ebb8
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-05-24 13:46:57 -0300

    feat: ensure create category validate duplicated name

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -27,4 +27,9 @@ export class CategoriesRespository {
   list(): Category[] {
     return this.categories;
   }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+    return category;
+  }
 }
