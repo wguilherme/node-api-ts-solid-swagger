41c57ca6854e5f1e60040b2757b80526abc97ef3
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-05-24 10:54:07 -0300

    feat: add categories route

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express'
+import { categoriesRoutes } from './routes/categories.routes'
 
 const app = express()
 app.use(express.json())
+
+app.use(categoriesRoutes)
+
+
 
 app.get('/', (req, res)=>res.json('works'))
 
