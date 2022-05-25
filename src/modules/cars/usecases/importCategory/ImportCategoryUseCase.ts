07e2a4eeb92704a12b2b1590a63081f2435cb31d
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-05-25 08:42:39 -0300

    feat: add node stream and parse csv

--- src/modules/cars/usecases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/usecases/importCategory/ImportCategoryUseCase.ts
@@ -1,5 +1,11 @@
+import { parse } from "csv-parse";
+import fs from "fs";
+
 export class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = fs.createReadStream(file.path);
+    const parseFile = parse();
+    stream.pipe(parseFile);
+    parseFile.on("data", async (line) => console.log(line));
   }
 }
